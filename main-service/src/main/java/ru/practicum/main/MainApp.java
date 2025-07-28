8bcbe7b79d382b3a8e021fd7a0b577c368897916
alxb <alx.b2la@gmail.com>
2025-07-28 18:46:04 +0300

    Add common files

--- main-service/src/main/java/ru/practicum/main/MainApp.java
+++ main-service/src/main/java/ru/practicum/main/MainApp.java
@@ -2,8 +2,10 @@ package ru.practicum.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"ru.practicum"})
 public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
